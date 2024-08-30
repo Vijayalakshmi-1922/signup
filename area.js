1cc4578183028b5e74a71badb19c599160b5ce96
Vijayalakshmi-1922 <95222203093@psncet.ac.in>
2024-08-30 15:01:00 +0530

    Add files via upload

---
+++ area.js
@@ -0,0 +1,24 @@
+const PI=3.14;
+let r=7;
+let areaofcircle =PI*r*r;
+console.log("Area of Circle is:",areaofcircle);
+
+let b=4;
+let h=3;
+let areaoftriangle=1/2*b*h;
+console.log("Area of Triangle is:",areaoftriangle);
+
+let l=4;
+let w=7;
+let areaofrectangle=l*w;
+console.log("Area of Reactangle is:",areaofrectangle);
+
+let s=4;
+let square=s*s;
+console.log("Area of Square is:",square);
+
+let radius=3;
+let height=4;
+let cylinder=2*PI*radius*(radius+height);
+console.log("Area of Cylinder is:",cylinder);
+
